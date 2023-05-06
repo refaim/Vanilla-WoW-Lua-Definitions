2f43cf31a15ab42e156c4670c9730e90a33052fc
Roman Kharitonov <rkharito@yandex.ru>
2023-05-06 20:36:29 +1000

    Import initial state

---
+++ stl.aliases.def.lua
@@ -0,0 +1,39 @@
+--- Returns a formatted version of its variable number of arguments following
+--- the description given in its first argument (which must be a string). The
+--- format string follows the same rules as the ISO C function `sprintf`. The
+--- only differences are that the options/modifiers `*`, `h`, `L`, `l`, `n`, and
+--- `p` are not supported and that there is an extra option, `q`.
+---
+--- The `q` option formats booleans, nil, numbers, and strings in a way that the
+--- result is a valid constant in Lua source code. Booleans and nil are written
+--- in the obvious way (`true`, `false`, `nil`). Floats are written in
+--- hexadecimal, to preserve full precision. A string is written between double
+--- quotes, using escape sequences when necessary to ensure that it can safely
+--- be read back by the Lua interpreter. For instance, the call
+---
+--- format('%q', 'a string with "quotes" and \n new line') may produce
+--- the string:
+---
+--- > "a string with \"quotes\" and \
+--- > new line"
+---
+--- The options `A`, `a`, `E`, `e`, `f`, `g`, `G` and `g` all expect a number as
+--- argument. Options `c`, `d`, `i`, `o`, `u`, `X`, and `x` expect an integer.
+--- When Lua is compiled with a C89 compiler, options `A` and `a` (hexadecimal
+--- floats) do not support any modifier (flags, width, length).
+---
+--- Option `s` expects a string; if its argument is not a string, it is
+--- converted to one following the same rules of `tostring`. If the option
+--- has any modifier (flags, width, length), the string argument should not
+--- contain embedded zeros.
+--- @param formatstring string
+--- @return string
+function format(formatstring, ...) end
+
+---
+--- Receives a string and returns a copy of this string with all uppercase
+--- letters changed to lowercase. All other characters are left unchanged. The
+--- definition of what an uppercase letter is depends on the current locale.
+--- @param s string
+--- @return string
+function strlower(s) end
